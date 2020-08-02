e94bd9dc2be8a821570bc36db87677b1fc41d2d9
gmargonato <gabriel-merida@hotmail.com>
2020-08-02 19:59:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #desenvolvido por gmargonato
-#inicio do projeto: 08/junho/2020
+#inicio da construcao: 08/junho/2020
 
 import os
 import subprocess
@@ -22,37 +22,78 @@ def get_pixel_color(posX,posY):
 ##########################################################
 #Seletor de scripts com base no input do usuario
 def seletor_scripts():
-    lista_scripts = ("nada selecionado", "rook_psc", "cyc_thais","stonerefiner")
+    lista_scripts = ("nada selecionado", "rook_psc", "cyc_thais","stonerefiner","forest_fury","rook_wasp")
     selected = select("Selecione um script da lista","Seletor de Scripts", options = lista_scripts)
 
-    global modulo
-    
+    global script_selecionado
+ 
     if selected == lista_scripts[0]:
         popup("Voce nao escolheu nenhum script - Finalizando!")
         type('c', KeyModifier.CMD + KeyModifier.SHIFT)
         
     if selected == lista_scripts[1]:
-        print "Script Selecionado: rook_psc"
-        modulo = importlib.import_module("rook_psc")
+        script_selecionado = "rook_psc"
         configs_script()
     
     if selected == lista_scripts[2]:
-        print "Script Selecionado: cyc_thais"
-        modulo = importlib.import_module("cyc_thais")
+        script_selecionado = "cyc_thais"
         configs_script()
 
     if selected == lista_scripts[3]:
-        print "Script Selecionado: stonerefiner"
-        modulo = importlib.import_module("stonerefiner")
+        script_selecionado = "stonerefiner"
+        configs_script()
+
+    if selected == lista_scripts[4]:
+        script_selecionado = "forest_fury"
+        configs_script()   
+
+    if selected == lista_scripts[5]:
+        script_selecionado = "rook_wasp"
         configs_script()
 
 #configuracoes basicas do script importado
 def configs_script():
-    global loot; global drop; global wp_final; global equip_ring;
+    global loot; global drop; global wp_final; global equip_ring;global modulo
+    
+    print "Script Selecionado:",script_selecionado        
+    modulo = importlib.import_module(script_selecionado)     
     loot = modulo.loot
     drop = modulo.drop
     wp_final = modulo.wp_final
     equip_ring = modulo.equip_ring
+    
+##########################################################
+def dropar():
+    print "dropando items..."
+    
+    if script_selecionado == "rook_psc":
+        if exists("smallaxe.png"):dropar_item("smallaxe.png","small axe")
+        if exists("leatherhelmet.png"):dropar_item("leatherhelmet.png","leather helmet")
+        if exists("spear.png"):dropar_item("spear.png","spear")
+    if script_selecionado == "rook_wasp":
+        if exists("1596220559045.png"): dropar_item("1596220559045.png","hatchet")
+        if exists("1596220591667.png"): dropar_item("1596220591667.png","viking helmet")
+        if exists("1596220607452.png"): dropar_item("1596220607452.png","studded shield")
+        if exists("1596220626287.png"): dropar_item("1596220626287.png","bone")
+        if exists("1596221479773.png"): dropar_item("1596221479773.png","studded helmet")
+        if exists("1596221994953.png"): dropar_item("1596221994953.png","heavy tome")
+        if exists("1596222023378.png"): dropar_item("1596222023378.png","studded armor")
+        if exists(Pattern("1596223046626.png").similar(0.80)): dropar_item(Pattern("1596223046626.png").similar(0.80),"torch")
+        if exists("1596223057218.png"): dropar_item("1596223057218.png","pelvis bone")
+        if exists("1596223848531.png"): dropar_item("1596223848531.png","mace")
+        
+
+    print "todos os items dropados"
+
+def dropar_item(sprite_item,item_name):
+    #vida() #para curar minha vida enquanto estou dropando
+    if exists(sprite_item):
+        imageCount = len(list([x for x in findAll(sprite_item)]))
+        for i in range(imageCount):
+            print "dropando",item_name,i+1,"de",imageCount
+            dragDrop(sprite_item, Location(550, 375))
+            wait(1)
+    else:return
 
 ##########################################################
 #função recuperar a vida      
@@ -83,7 +124,7 @@ def statusBar_check():
     statusBar = Region(499,82,106,13)
     
     if statusBar.exists("food.png"):type(food_htk)
-    #if statusBar.exists().type(poison_spell_htk)
+    if statusBar.exists("poison.png"):type(poison_spell_htk)
     else: return
 
 ##########################################################
@@ -124,9 +165,9 @@ def atacar():
         return
     
     else:
-        type(Key.SPACE)
         print "detectado monstro na tela, atacando"
-        atacando(0)
+        type(Key.SPACE)        
+        atacando()
         if loot == 1:
             if loot_type == "auto": loot_automatico()
             if loot_type == "manual": loot_manual()
@@ -134,29 +175,22 @@ def atacar():
  
 ##########################################################
 #função verificar se estou atacando
-def atacando(atkCount):
+def atacando():
 
     vida()
     mana()
     
     if use_spell == 1: magia_atk(6)
-
-    if atkCount > 30:
-        type(Key.SPACE)
-        print "[ATENCAO] trocando de alvo"
-        wait(5)
-        atacando(0)
-
+    
     output = get_pixel_color(932,60)
 
     if (output == 'ff0000' or output == 'ff7f7d'): #vermelho ou vermelho-claro
-        atkCount+=1
         wait(1)
-        atacando(atkCount)
+        atacando()
 
     if output == 'ffffff': #branco
        type(Key.SPACE)
-       atacando(atkCount)
+       atacando()
         
     else:return
 
@@ -175,7 +209,6 @@ def changeHandler(event):
 
 def cheguei_wp(tempo_parado):
     while True:
-        vida() #enquanto anda, verifica a vida
         nav = Region(1111,47,110,115)
         nav.onChange(1,changeHandler)
         nav.somethingChanged = False
@@ -240,7 +273,7 @@ rope_htk = "o"
 shovel_htk = "p"
 ring_htk = "l"
 
-wp = 1
+wp = 4
 use_spell = 0
 loot_type = "auto"
 
@@ -252,6 +285,7 @@ while True:
 
     atacar()
     statusBar_check()
+    atacar()
     
     if equip_ring == 1: check_ring()
 
@@ -260,8 +294,15 @@ while True:
         print "[ERRO] Waypoint", wp,"nao encontrado!"    
         pass
 
-    #dropa o item no ultimo Wp
-    if wp == wp_final and drop == 1: modulo.dropar()
+    #verificacoes no ultimo wp
+    if wp == wp_final: 
+
+        #verifica se deve dropar items
+        if drop == 1: dropar()
+        
+        #verifica se deve sair da hunt
+        try: print "verificando status..."; modulo.check_exit()
+        except: print "metodo check_exit nao existe!";pass
     
     wp+=1
     if wp > wp_final: wp = 1
